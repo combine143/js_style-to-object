1b6dd6a0d19e433778010e081ee6ded3ff3c1219
combine143 <64581819+combine143@users.noreply.github.com>
2025-04-20 15:13:46 +0300

    Update convertToObject.js2

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -9,7 +9,7 @@ function convertToObject(sourceString) {
   const result = {};
 
   sourceString.split(';').forEach((line) => {
-    const [key, value] = line.split(':');
+    const [key, value] = line.split(':', 2);
 
     if (key && value !== undefined) {
       result[key.trim()] = value.trim();
